7b4dff4dba30e6af64f6124c8cf12e0ad8a3c470
Kevin Lascano <kevinjunior-08@hotmail.com>
2026-06-20 14:35:43 -0500

    Complete mi prgrama

---
+++ biblioteca_app/modelos/usuario.py
@@ -0,0 +1,11 @@
+class Usuario:
+    
+    def __init__(self, nombre, email):
+        self.nombre = nombre
+        self.email = email
+    
+    def mostrar_informacion(self):
+        return f"Nombre: {self.nombre}\nEmail: {self.email}"
+
+    def __str__(self):
+        return f"{self.nombre} ({self.email})"
